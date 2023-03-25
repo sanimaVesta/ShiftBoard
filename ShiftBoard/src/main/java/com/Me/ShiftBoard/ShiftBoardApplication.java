a02d22c43717af336442efea8b61f33af36c49e6
anima <sahilkalathiya0120@gmail.com>
2023-03-25 16:11:06 -0400

    Initial commit Create Project

---
+++ ShiftBoard/src/main/java/com/Me/ShiftBoard/ShiftBoardApplication.java
@@ -0,0 +1,13 @@
+package com.Me.ShiftBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiftBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiftBoardApplication.class, args);
+	}
+
+}
